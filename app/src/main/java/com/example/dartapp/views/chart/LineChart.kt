016c277a2f38c299bd8a04c202f5522a4d0eae4a
FelberMartin <felber.martin3@gmail.com>
2021-10-11 16:40:36 +0200

    simple animation for linechart

--- app/src/main/java/com/example/dartapp/views/chart/LineChart.kt
+++ app/src/main/java/com/example/dartapp/views/chart/LineChart.kt
@@ -1,14 +1,16 @@
 package com.example.dartapp.views.chart
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
+import android.os.Build
 import android.util.AttributeSet
 import android.util.Log
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
+import androidx.annotation.RequiresApi
 import androidx.core.graphics.minus
 import androidx.core.graphics.plus
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import com.example.dartapp.R
 import com.google.android.material.color.MaterialColors
 
@@ -20,26 +22,40 @@ class LineChart @JvmOverloads constructor(
 
     private var linePath = Path()
     var smoothedLine = true
+    var animated = true
+    private var frame = 0
+    private var interpolator = AccelerateDecelerateInterpolator()
+
+    val textShader: Shader = LinearGradient(0f, 0f, 0f, 500f, intArrayOf(
+        Color.parseColor("#F97C3C"),
+        Color.parseColor("#FDB54E"),
+        Color.parseColor("#64B678"),
+        Color.parseColor("#478AEA"),
+        Color.parseColor("#8446CC")
+    ), null, Shader.TileMode.CLAMP)
 
     private val linePaint = Paint().apply {
         isAntiAlias = true
         isDither = true
         style = Paint.Style.STROKE
-        strokeWidth = 5f
+        strokeWidth = 10f
         strokeCap = Paint.Cap.ROUND
         strokeJoin = Paint.Join.ROUND
         color = MaterialColors.getColor(this@LineChart, R.attr.colorPrimary)
+        shader = textShader
     }
 
     init {
         if (isInEditMode) {
             drawCoordRect = true
+            animated = false
         }
         data = DataSet.random(count = 8, randomX = true)
     }
 
     fun reload() {
         dataChanged()
+        frame = 0
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -54,6 +70,7 @@ class LineChart @JvmOverloads constructor(
         data.forEach { dp -> Log.d(TAG, "${dp.x}, ${dp.y}") }
 
         updatePath()
+        frame = 0
         invalidate()
     }
 
@@ -80,17 +97,45 @@ class LineChart @JvmOverloads constructor(
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         drawLines(canvas)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun drawLines(canvas: Canvas) {
-        canvas.drawPath(linePath, linePaint)
+        val animationEnd = if (animated) 50f else 0f
+        if (frame >= animationEnd)
+            canvas.drawPath(linePath, linePaint)
+        else {
+            val fraction = interpolator.getInterpolation(frame/animationEnd)
+            val appr = linePath.approximate(0.5f)
+            val points = appr.filterIndexed { index, _ -> index % 3 != 0 }
+            val fractions = appr.filterIndexed { index, _ -> index % 3 == 0 }
+            var lines = ArrayList<Float>()
+            for (i in 0 until points.size / 2) {
+                val x = points[2 * i]
+                val y = points[2 * i + 1]
+                lines.add(x)
+                lines.add(y)
+                if (i != 0 && i != points.size / 2 - 1) {
+                    lines.add(x)
+                    lines.add(y)
+                }
+                if (fractions[i] > fraction)
+                    break
+            }
+            canvas.drawLines(lines.toFloatArray(), linePaint)
+            frame++
+            invalidate()
+        }
+
+        // Circles around data points
         for (i in 0 until data.size) {
             val p = inCoordSystem(i)
-            canvas.drawCircle(p.x, p.y, 5f, linePaint)
+//            canvas.drawCircle(p.x, p.y, 5f, linePaint)
         }
     }
 }
