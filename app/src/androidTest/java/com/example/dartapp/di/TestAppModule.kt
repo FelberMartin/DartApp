0fe1943d43b409ab280d4689eca94e275acb41d6
Felber Martin <martin.felber@app-stone.org>
2022-07-19 14:32:36 +0200

    Updated gradle

--- app/src/androidTest/java/com/example/dartapp/di/TestAppModule.kt
+++ app/src/androidTest/java/com/example/dartapp/di/TestAppModule.kt
@@ -6,12 +6,12 @@ import com.example.dartapp.database.LegDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Named
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object TestAppModule {
 
     @Provides
