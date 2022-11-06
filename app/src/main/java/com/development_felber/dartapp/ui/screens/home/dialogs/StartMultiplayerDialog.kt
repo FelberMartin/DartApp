4c90f94db2a2faf407a84b6a1128e4e62b9b4648
Felber Martin <martin.felber@app-stone.org>
2022-11-06 21:25:33 +0100

    Started StartMultiplayerDialog.kt

--- app/src/main/java/com/development_felber/dartapp/ui/screens/home/dialogs/StartMultiplayerDialog.kt
+++ app/src/main/java/com/development_felber/dartapp/ui/screens/home/dialogs/StartMultiplayerDialog.kt
@@ -1,11 +1,17 @@
 package com.development_felber.dartapp.ui.screens.home.dialogs
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.filled.SwapHoriz
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.development_felber.dartapp.ui.theme.DartAppTheme
 
 data class Player(
     val name: String
@@ -27,19 +33,34 @@ fun StartMultiplayerDialog(
     onStart: (StartMultiplayerDialogResult) -> Unit,
     onCancel: () -> Unit,
 ) {
+    val legCount = remember { mutableStateOf(3) }
+    val setCount = remember { mutableStateOf(1) }
+
     AlertDialog(
         onDismissRequest = { },
         text = {
             Column() {
-
+                LegAndSetSelection(
+                    legCount = legCount,
+                    setCount = setCount,
+                )
             }
         },
         confirmButton = {
             CancelConfirmButtonRow(
                 onCancel = onCancel,
                 onConfirm = {
-
-                })
+                    onStart(
+                        StartMultiplayerDialogResult(
+                            // TODO: Get player1 and player2
+                            player1 = lastPlayer1 ?: allPlayers.first(),
+                            player2 = lastPlayer2 ?: allPlayers.first(),
+                            legCount = legCount.value,
+                            setCount = setCount.value,
+                        )
+                    )
+                },
+            )
         }
     )
 }
@@ -62,26 +83,223 @@ private fun CancelConfirmButtonRow(
     }
 }
 
+@Composable
+private fun ColumnScope.PlayersSelection(
+    allPlayers: List<Player>,
+    player1: Player?,
+    player2: Player?,
+    onPlayer1Changed: (Player?) -> Unit,
+    onPlayer2Changed: (Player?) -> Unit,
+    onNewPlayerCreated: (String) -> Unit,
+) {
+    HeaderText(text = "Players")
+
+    PlayerSelectionInfoRow()
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        PlayerSelection(
+            allPlayers = allPlayers,
+            lastPlayer = lastPlayer1,
+            onNewPlayerCreated = onNewPlayerCreated,
+        )
+
+        PlayerSwap {
+            onPlayer1Changed(player2)
+            onPlayer2Changed(player1)
+        }
+
+        PlayerSelection(
+            allPlayers = allPlayers,
+            lastPlayer = lastPlayer2,
+            onNewPlayerCreated = onNewPlayerCreated,
+        )
+    }
+}
+
+@Composable
+private fun HeaderText(text: String) = Text(
+    text = text,
+    style = MaterialTheme.typography.headlineSmall,
+)
+
+
+@Composable
+private fun PlayerSelectionInfoRow() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        PlayerSelectionInfoRowText(text = "Player 1")
+        PlayerSelectionInfoRowText(text = "vs.")
+        PlayerSelectionInfoRowText(text = "Player 2")
+    }
+}
+
+@Composable
+private fun PlayerSelectionInfoRowText(
+    text: String,
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.labelLarge,
+        color = MaterialTheme.colorScheme.secondary,
+    )
+}
+
+@Composable
+private fun PlayerSelection(
+    selectedPlayer: Player?,
+    onSelectedPlayerChanged: (Player) -> Unit,
+    allPlayers: List<Player>,
+    onNewPlayerCreated: (String) -> Unit,
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    val playerNames = allPlayers.map { it.name }
+
+    DropdownMenu(
+        expanded = isExpanded,
+        onDismissRequest = { isExpanded = false },
+    ) {
+        DropdownMenuItem(
+            onClick = {
+                isExpanded = false
+                onNewPlayerCreated("New Player")
+            }
+        ) {
+            Text("New Player")
+        }
+
+        DropdownMenuItem(
+            onClick = {
+                isExpanded = false
+                onNewPlayerCreated("New Player 2")
+            }
+        ) {
+            Text("New Player 2")
+        }
+
+        DropdownMenuItem(
+            onClick = {
+                isExpanded = false
+                onNewPlayerCreated("New Player 3")
+            }
+        ) {
+            Text("New Player 3")
+        }
+
+        DropdownMenuItem(
+            onClick = {
+                isExpanded = false
+                onNewPlayerCreated("New Player 4")
+            }
+        ) {
+            Text("New Player 4")
+        }
+
+        DropdownMenuItem(
+            onClick = {
+                isExpanded = false
+                onNewPlayerCreated("New Player 5")
+            }
+        ) {
+            Text("New Player 5")
+        }
+    }
+
+    OutlinedTonalButton(
+        onClick = { isExpanded = true },
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Text(
+            text = selectedPlayer?.name ?: "Select Player",
+            style = MaterialTheme.typography.bodyLarge,
+        )
+    }
+}
+
+@Composable
+private fun PlayerSwap(
+    onClick: () -> Unit,
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Default.SwapHoriz,
+            contentDescription = "Swap players",
+        )
+    }
+}
+
 
 @Composable
 private fun ColumnScope.LegAndSetSelection(
     legCount: MutableState<Int>,
     setCount: MutableState<Int>,
 ) {
-    Text(
-        text = "Game Settings",
-        style = MaterialTheme.typography.headlineMedium
+    HeaderText(text = "Game Settings")
+
+    StepperRow(
+        text = "Legs to win a Set",
+        value = legCount.value,
+        onValueChanged = { legCount.value = it },
     )
 
-    // TODO: Maybe use segmented buttons here as well, instead of steppers
+    StepperRow(
+        text = "Sets to win the game",
+        value = setCount.value,
+        onValueChanged = { setCount.value = it },
+    )
 }
 
 @Composable
 fun StepperRow(
     text: String,
     value: Int,
+    onValueChanged: (Int) -> Unit,
     minValue: Int = 1,
     maxValue: Int = 5
 ) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.secondary
+        )
 
+        Spacer(Modifier.width(8.dp))
+
+        Stepper(
+            value = value,
+            onValueChange = onValueChanged,
+            minValue = minValue,
+            maxValue = maxValue
+        )
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 380, heightDp = 700)
+@Composable
+private fun StartMultiplayerDialogPreview() {
+    DartAppTheme() {
+        StartMultiplayerDialog(
+            allPlayers = listOf(
+                Player("Player 1"),
+                Player("Player 2"),
+                Player("Player 3"),
+                Player("Player 4"),
+            ),
+            lastPlayer1 = null,
+            lastPlayer2 = null,
+            onNewPlayerCreated = {},
+            onStart = {},
+            onCancel = {},
+        )
+    }
 }
---
+++ app/src/main/java/com/development_felber/dartapp/ui/screens/home/dialogs/Stepper.kt
@@ -0,0 +1,88 @@
+package com.development_felber.dartapp.ui.screens.home.dialogs
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.development_felber.dartapp.ui.theme.DartAppTheme
+
+@Composable
+fun Stepper(
+    value: Int,
+    onValueChange: (Int) -> Unit,
+    minValue: Int,
+    maxValue: Int
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        StepperButton(
+            onClick = {
+                if (value > minValue) {
+                    onValueChange(value - 1)
+                }
+            },
+        ) {
+            Icon(
+                imageVector = Icons.Default.Remove,
+                contentDescription = "Decrease value"
+            )
+        }
+
+        Text(
+            text = value.toString(),
+            style = MaterialTheme.typography.headlineSmall,
+        )
+
+        StepperButton(
+            onClick = {
+                if (value < maxValue) {
+                    onValueChange(value + 1)
+                }
+            },
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Increase value"
+            )
+        }
+    }
+}
+
+@Composable
+private fun StepperButton(
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+        contentPadding = PaddingValues(0.dp),
+    ) {
+        content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun StepperPreview() {
+    DartAppTheme {
+        Stepper(
+            value = 3,
+            onValueChange = { },
+            minValue = 1,
+            maxValue = 5
+        )
+    }
+}
