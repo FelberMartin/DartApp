aa3d3648cec8c21f9f0486d0403b7b28e24cf5a6
Martin Felber <martin.felber@app-stone.org>
2023-01-20 14:25:47 +0100

    Added tests for game/set finished dialogs

--- app/src/test/java/com/development_felber/dartapp/ui/screens/game/MultiplayerGameViewModelTest.kt
+++ app/src/test/java/com/development_felber/dartapp/ui/screens/game/MultiplayerGameViewModelTest.kt
@@ -16,18 +16,32 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class MultiplayerGameViewModelTest : GameViewModelTest() {
 
-    private val player1 =  Player("Player 1")
-    private val player2 = Player("Player 2")
+    companion object {
+        private val player1 =  Player("Player 1")
+        private val player2 = Player("Player 2")
+        private const val setsToWin = 3
+        private const val legsToWinSet = 3
+    }
 
     @Before
     override fun setup() {
-        setupGameViewModel(gameSetup = GameSetup.Multiplayer(
-            player1 = player1,
-            player2 =  player2,
-            setsToWin = 3,
-            legsToWinSet = 3,
-        ))
+        setupGameViewModel(gameSetup = createGameSetup())
     }
+
+    private fun createGameSetup(
+        player1: Player = Companion.player1,
+        player2: Player = Companion.player2,
+        setsToWin: Int = Companion.setsToWin,
+        legsToWinSet: Int = Companion.legsToWinSet,
+    ) = GameSetup.Multiplayer(
+        player1 = Companion.player1,
+        player2 = Companion.player2,
+        setsToWin = Companion.setsToWin,
+        legsToWinSet = Companion.legsToWinSet,
+    )
+
+
+    // --------------- Dialogs -------------
 
     @Test
     fun `player one finishes leg, show LegWonDialog after entering double and checkout`() = runHotFlowTest {
@@ -70,14 +84,72 @@ class MultiplayerGameViewModelTest : GameViewModelTest() {
     }
 
     @Test
+    fun `after finishing set, show set finished dialog`() = runHotFlowTest {
+        repeat(legsToWinSet) {
+            endGame(winner = PlayerRole.One)
+            delay(1)
+        }
+        val dialog = viewModel.gameUiState.value.dialogToShow
+        assertThat(dialog).isEqualTo(GameDialogManager.DialogType.SetJustFinished)
+    }
+
+    @Test
+    fun `after winning all sets, show game finished dialog`() = runHotFlowTest {
+        setupGameViewModel(gameSetup = createGameSetup(setsToWin = 1))
+        repeat(legsToWinSet) {
+            endGame(winner = PlayerRole.One)
+            delay(1)
+        }
+        val dialog = viewModel.gameUiState.value.dialogToShow
+        assertThat(dialog).isEqualTo(GameDialogManager.DialogType.GameFinished)
+    }
+
+
+
+    // ---------- Saving to Database -------------
+
+    @Test
+    fun `after finishing a game with only one leg to win, a leg gets stored for each player`() = runTest {
+        setupGameViewModel(gameSetup = createGameSetup(
+            legsToWinSet = 1,
+            setsToWin = 1,
+        ))
+        endGame()
+        delay(1)
+        val legs = finishedLegDao.getAllLegs().getOrAwaitValueTest()
+        val player1Leg = legs.firstOrNull { it.playerName == Companion.player1.name }
+        assertThat(player1Leg).isNotNull()
+        val player2Leg = legs.firstOrNull { it.playerName == player2.name }
+        assertThat(player2Leg).isNotNull()
+    }
+
+    @Test
+    fun `do not save legs, if the game is still in progress`() = runTest {
+        endGame()
+        delay(1)
+        val savedLegs = finishedLegDao.getAllLegs().getOrAwaitValueTest()
+        assertThat(savedLegs).isEmpty()
+    }
+
+    // TODO: Save Sets and Multiplayer entries
+
+
+    // ------------ Others ---------------
+
+    @Test
     fun `after finishing leg, it is added to previous legs list`() = runHotFlowTest {
         endGame()
         val previousLegs = viewModel.gameState.currentPlayerGameState.previousLegs
         assertThat(previousLegs).hasSize(1)
     }
 
-    private suspend fun endGame() {
-        enterServes(listOf(180, 0, 180, 0, 141))
+    private suspend fun endGame(winner: PlayerRole = PlayerRole.One) {
+        var serves = mutableListOf(180, 0, 180, 0, 141)
+        val startingPlayer = viewModel.gameState.currentPlayerRole
+        if (startingPlayer != winner) {
+            serves.add(0, 0)
+        }
+        enterServes(serves)
         delay(1)
         viewModel.doubleAttemptsAndCheckoutConfirmed(1, 3)
         viewModel.onContinueInDialogClicked()
@@ -92,32 +164,4 @@ class MultiplayerGameViewModelTest : GameViewModelTest() {
         assertThat(viewModel.gameState.currentPlayerRole).isEqualTo(PlayerRole.One)
     }
 
-    // ---------- Saving to Database -------------
-
-    @Test
-    fun `after finishing a game with only one leg to win, a leg gets stored for each player`() = runTest {
-        setupGameViewModel(gameSetup = GameSetup.Multiplayer(
-            player1 = player1,
-            player2 = player2,
-            legsToWinSet = 1,
-            setsToWin = 1,
-        ))
-        endGame()
-        delay(1)
-        val legs = finishedLegDao.getAllLegs().getOrAwaitValueTest()
-        val player1Leg = legs.firstOrNull { it.playerName == player1.name }
-        assertThat(player1Leg).isNotNull()
-        val player2Leg = legs.firstOrNull { it.playerName == player2.name }
-        assertThat(player2Leg).isNotNull()
-    }
-
-    @Test
-    fun `do not save legs, if the game is still in progress`() = runTest {
-        endGame()
-        delay(1)
-        val savedLegs = finishedLegDao.getAllLegs().getOrAwaitValueTest()
-        assertThat(savedLegs).isEmpty()
-    }
-
-    // Save Sets and Multiplayer entries
 }
